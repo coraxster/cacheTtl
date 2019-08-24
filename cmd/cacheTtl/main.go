5da37623e1d0e5a6429b09a2bd8aaa329d222184
dmitry kuzmin <corax@list.ru>
2019-08-24 16:22:47 +0300

    simple cli

--- cmd/cacheTtl/main.go
+++ cmd/cacheTtl/main.go
@@ -32,16 +32,9 @@ func main() {
 		keyStart := 4
 		switch strings.ToUpper(line[:keyStart]) {
 		case "SET ":
-			KvLine := line[keyStart:]
-			keyEnd := strings.Index(KvLine, " ")
-			if keyEnd == -1 {
-				logger.Println("not found key end")
-				continue
-			}
-			key := KvLine[:keyEnd]
-			value := KvLine[keyEnd+1:]
-			if err := cache.Set(key, value, time.Now().Add(ttl)); err != nil {
-				logger.Println("error with saving: ", err)
+			kvLine := line[keyStart:]
+			if err := set(kvLine, cache); err != nil {
+				logger.Println("error with set: ", err)
 				continue
 			}
 			out.WriteString("set\n")
@@ -61,53 +54,14 @@ func main() {
 			}
 			out.WriteString("del\n")
 		case "LOAD":
-			file, err := os.Open(persistFile)
-			if err != nil {
-				logger.Println("error with opening data.dat file: ", err)
+			if err := load(cache); err != nil {
+				logger.Println("error with loading: ", err)
 				continue
-			}
-			dec := gob.NewDecoder(bufio.NewReader(file))
-			var el PersistEl
-			for {
-				err := dec.Decode(&el)
-				if err == io.EOF {
-					break
-				}
-				if err != nil {
-					logger.Println("error with loading data.dat file: ", err)
-					continue
-				}
-				if err := cache.Set(el.Key, el.Val, el.Ttl); err != nil {
-					logger.Println("error with saving: ", err)
-					continue
-				}
 			}
 			out.WriteString("load\n")
 		case "SAVE":
-			file, err := os.Create(persistFile)
-			if err != nil {
-				logger.Println("error with opening data.dat file: ", err)
-				continue
-			}
-			b := bufio.NewWriter(file)
-			enc := gob.NewEncoder(b)
-			fn := func(key string, val interface{}, ttl time.Time) error {
-				valStr := val.(string) // always string
-				el := PersistEl{
-					Key: key,
-					Val: valStr,
-					Ttl: ttl,
-				}
-				if err := enc.Encode(&el); err != nil {
-					return errors.New("error with encoding:" + err.Error())
-				}
-				return nil
-			}
-			err = cache.Walk(fn)
-			b.Flush()
-			file.Close()
-			if err != nil {
-				logger.Println("error with saving data.dat file: ", err)
+			if err := save(cache); err != nil {
+				logger.Println("error with loading: ", err)
 				continue
 			}
 			out.WriteString("save\n")
@@ -118,8 +72,63 @@ func main() {
 	}
 }
 
+func set(kvLine string, cache *cacheTtl.Cache) error {
+	keyEnd := strings.Index(kvLine, " ")
+	if keyEnd == -1 {
+		return errors.New("not found key end")
+	}
+	key := kvLine[:keyEnd]
+	value := kvLine[keyEnd+1:]
+	return cache.Set(key, value, time.Now().Add(ttl))
+}
+
 type PersistEl struct {
 	Key string
 	Val string
 	Ttl time.Time
+}
+
+func load(cache *cacheTtl.Cache) error {
+	file, err := os.Open(persistFile)
+	if err != nil {
+		return errors.New("error with opening data.dat file: " + err.Error())
+	}
+	dec := gob.NewDecoder(bufio.NewReader(file))
+	var el PersistEl
+	for {
+		err := dec.Decode(&el)
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return errors.New("error with loading data.dat file: " + err.Error())
+		}
+		if err := cache.Set(el.Key, el.Val, el.Ttl); err != nil {
+			return errors.New("error with set:" + err.Error())
+		}
+	}
+}
+
+func save(cache *cacheTtl.Cache) error {
+	file, err := os.Create(persistFile)
+	if err != nil {
+		return errors.New("error with opening data.dat file: " + err.Error())
+	}
+	defer file.Close()
+	b := bufio.NewWriter(file)
+	defer b.Flush()
+	enc := gob.NewEncoder(b)
+	fn := func(key string, val interface{}, ttl time.Time) error {
+		valStr := val.(string) // always string
+		el := PersistEl{
+			Key: key,
+			Val: valStr,
+			Ttl: ttl,
+		}
+		if err := enc.Encode(&el); err != nil {
+			return errors.New("error with encoding:" + err.Error())
+		}
+		return nil
+	}
+	return cache.Walk(fn)
 }
